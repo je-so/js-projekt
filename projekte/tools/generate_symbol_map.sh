2c217dcda9dd42476c53592e9807f4ef6bca94b5
Jörg Seebohn <je-so@basis-lager.(none)>
2014-03-07 17:30:51 +0100

    Add comment to generate_symbol_map

--- projekte/tools/generate_symbol_map.sh
+++ projekte/tools/generate_symbol_map.sh
@@ -6,6 +6,11 @@ fi
 
 outfile="$1".symbol.map
 
-# extract all static [t] and global [T] text symbols
-# some symbols have are named name.xxx.0: remove trailing .xxx.0
+# - Extract all static [t] and global [T] text symbols
+# - Also some symbols are named name.constprop.X
+#   (a gcc thing I do not understand at this time).
+#   So let us remove all trailing ".xXxXxX"
 nm -S "$1" | sed -e "/[0-9a-f]\+ [0-9a-f]\+ [tT] /!d" | sed -e "s/\( [A-Za-z0-9_]\+\)\.[^ ]*$/\1/" | sort | uniq > "$outfile"
+
+# Symbolmap will be used in the crash handler in the future to print
+# a call stack dump with symbolic names instead of memory addresses
